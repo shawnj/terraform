6260566dfabb9ff1b75c52bbe8f37547605fb9e0
Shawn Junell <Shawn.Junell@pemco.com>
2018-03-16 14:55:24 -0700

    update

--- examples/aws_win_vm/main.tf
+++ examples/aws_win_vm/main.tf
@@ -14,6 +14,8 @@ provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
   region     = "${var.region}"
+
+  skip_credentials_validation = true
 }
 
 data "aws_ami" "amazon_windows" {
@@ -42,9 +44,9 @@ module "vpc" {
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = ["us-west-2a", "us-west-2b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   #enable_nat_gateway = true
   #enable_vpn_gateway = true
