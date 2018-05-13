8f5d2a2c493ebea008103797565613b3f56bcaf8
admin <admin@admins-MBP-2.hsd1.wa.comcast.net>
2018-05-12 19:08:10 -0700

    update

--- modules/awslinuxvm/provider.tf
+++ modules/awslinuxvm/provider.tf
@@ -1,8 +1,8 @@
 # Configure the AWS Provider
-provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "${var.region}"
+#provider "aws" {
+#  access_key = "${var.aws_access_key}"
+#  secret_key = "${var.aws_secret_key}"
+#  region     = "${var.region}"
 
-  skip_credentials_validation = true
-}
+#  skip_credentials_validation = true
+#}
