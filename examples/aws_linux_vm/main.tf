77e1de3a05a3b1d6dbf72f6a952e09e1830239d1
Shawn Junell <Shawn.Junell@pemco.com>
2018-05-15 10:42:55 -0700

    update

--- examples/aws_linux_vm/main.tf
+++ examples/aws_linux_vm/main.tf
@@ -23,7 +23,7 @@ variable "keypair_name" {
 }
 
 provider "aws" {
- access_key = "${var.aws_access_key}"
+  access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
   region     = "${var.region}"
 
