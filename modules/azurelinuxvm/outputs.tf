b25efc19090c0c72ff23b4c9c24d24f723fa87ae
Shawn Junell <Shawn.Junell@pemco.com>
2018-06-06 08:13:34 -0700

    update Outputs

--- modules/azurelinuxvm/outputs.tf
+++ modules/azurelinuxvm/outputs.tf
@@ -1,9 +1,9 @@
-#output "ip" {
-#  value = "${azurerm_virtual_machine.vm.network_interface.0.ipv4_address}"
-#}
+output "public_ip" {
+  value = "${azurerm_public_ip.pip.ip_address}"
+}
 
-#output "vmName" {
-#  value = "${azurerm_virtual_machine.vm.name}"
-#}
+output "vmName" {
+  value = "${azurerm_virtual_machine.vm.name}"
+}
 
 
