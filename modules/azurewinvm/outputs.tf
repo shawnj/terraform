1d68c7c64fa28546df4deb5857e94eecedae1cca
Shawn Junell <sjunell@starbucks.com>
2018-08-21 08:56:08 -0700

    update

--- modules/azurewinvm/outputs.tf
+++ modules/azurewinvm/outputs.tf
@@ -1,9 +1,16 @@
-output "ip" {
-  value = "${azurerm_virtual_machine.vSphereVM.network_interface.0.ipv4_address}"
+output "network_interface_private_ip" {
+  description = "private ip addresses of the vm nics"
+  value       = "${azurerm_network_interface.nic.*.private_ip_address}"
 }
 
-output "vmName" {
-  value = "${azurerm_virtual_machine.vSphereVM.name}"
+output "public_ip_address" {
+  description = "The actual ip address allocated for the resource."
+  value       = "${azurerm_public_ip.pip.*.ip_address}"
+}
+
+output "public_ip_dns_name" {
+  description = "fqdn to connect to the first vm provisioned."
+  value       = "${azurerm_public_ip.pip.*.fqdn}"
 }
 
 
